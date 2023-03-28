70196d0046674f408b71f5e4e1611061825f5655
marco-lennartz <marco.lennartz@axa.de>
2023-03-28 15:24:14 +0200

    mychanges

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	n := time.Now()
+	fmt.Println("Dates and times", n)
 
-	fmt.Println("Dates and times")
+	t := time.Date(2023, time.March, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Println("Dates and times", t)
+	fmt.Println(t.Format(time.ANSIC))
 
+	parsedTime, _ := time.Parse(time.ANSIC, "Fri Mar 10 23:00:00 2023")
+	fmt.Printf("%T", parsedTime)
 }
