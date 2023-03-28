c682171991aff80e84ba89e4ba2d2c69d449f047
marco-lennartz <marco.lennartz@axa.de>
2023-03-28 15:52:38 +0200

    mychanges

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 11
+	var p = &anInt
+	fmt.Println("Value if p:", *p)
+
+	value1 := 42.42
+	pointer1 := &value1
+	fmt.Println("Value if pointer1:", *pointer1)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Value if pointer1:", *pointer1)
+	fmt.Println("Value if value1:", value1)
+
 }
