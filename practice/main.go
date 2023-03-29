6aa98fc73c5b9e164e0ef99fa1420569c288dfca
marco-lennartz <marco.lennartz@axa.de>
2023-03-29 13:50:53 +0200

    mychanges

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Blue"
+	colors[2] = "Green"
+	fmt.Println("Arrays", colors)
+
+	var numbers = [5]int{1, 2, 3, 4, 5}
+	fmt.Println("numbers", numbers)
+	fmt.Println("No of numbers", len(numbers))
 }
