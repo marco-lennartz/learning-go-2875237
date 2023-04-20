ce54861a4544b03f8eb993cc0ddee99f98aa8259
marco-lennartz <marco.lennartz@axa.de>
2023-04-20 09:16:51 +0200

    mychanges

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,26 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+	sum := addValues(1, 3)
+	fmt.Println(sum)
+	multiSum, multiCount := addAllValues(1, 3, 5, 6, 234, 21236)
+	fmt.Println("Sum of values", multiSum)
+	fmt.Println("Counted values", multiCount)
+}
+
+func doSomething() {
+	fmt.Println("doing something")
+}
+
+func addValues(v1 int, v2 int) int {
+	return v1 + v2
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0
+	for _, v := range values {
+		total += v
+	}
+	return total, len(values)
 }
