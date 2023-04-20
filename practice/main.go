5b751cfbc9199671fcbfd8b631a2e56092f9dd40
marco-lennartz <marco.lennartz@axa.de>
2023-04-20 09:00:59 +0200

    mychanges

--- practice/main.go
+++ practice/main.go
@@ -7,7 +7,27 @@ import (
 )
 
 func main() {
+
 	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(7) + 1
-	fmt.Println("Day", dow)
+	//fmt.Println("Day", dow)
+
+	var result string
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		result = "Sunday"
+	case 2:
+		result = "Monday"
+	case 3:
+		result = "Tuesday"
+	case 4:
+		result = "Wednesday"
+	case 5:
+		result = "Thursday"
+	case 6:
+		result = "Friday"
+	case 7:
+		result = "Saturday"
+
+	}
+	fmt.Println(result)
 }
