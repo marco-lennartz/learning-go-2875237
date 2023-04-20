8693efada16a216b7eec360d01b8fced66193033
marco-lennartz <marco.lennartz@axa.de>
2023-04-20 12:03:52 +0200

    mychanges

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Response type: %T\n", response)
+
+	defer response.Body.Close()
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	content := string(bytes)
+
+	fmt.Println(content)
 }
